e4e844de8383bab76c614b53795ec0681aac3175
Sergio Vargas <sergiolvargas95@gmail.com>
2025-09-14 12:44:45 -0500

    feat(domain): crea la entidad autor con sus reglas de negocio

--- src/main/java/co/edu/udec/bibliotecaDigital/domain/model/entities/Autor.java
+++ src/main/java/co/edu/udec/bibliotecaDigital/domain/model/entities/Autor.java
@@ -1,4 +1,63 @@
 package co.edu.udec.bibliotecaDigital.domain.model.entities;
 
 public class Autor {
+    private final Long id;
+    private final String primerNombre;
+    private final String segundoNombre;
+    private final String primerApellido;
+    private final String segundoApellido;
+    private final String nacionalidad;
+
+    public Autor(Long id, String primerNombre, String segundoNombre, String primerApellido, String segundoApellido, String nacionalidad) {
+        if(primerNombre == null || primerNombre.isBlank()) {
+            throw new IllegalArgumentException("El primer nombre no puede estar vacío");
+        }
+
+        if(primerApellido == null || primerApellido.isBlank()) {
+            throw new IllegalArgumentException("El primer apellido no puede estar vacío");
+        }
+
+        if(nacionalidad == null || nacionalidad.isBlank()) {
+            throw new IllegalArgumentException("La nacionalidad no puede estar vacía");
+        }
+
+        this.id = id;
+        this.primerNombre = primerNombre;
+        this.segundoNombre = segundoNombre;
+        this.primerApellido = primerApellido;
+        this.segundoApellido = segundoApellido;
+        this.nacionalidad = nacionalidad;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getPrimerNombre() {
+        return primerNombre;
+    }
+
+    public String getSegundoNombre() {
+        return segundoNombre;
+    }
+
+    public String getPrimerApellido() {
+        return primerApellido;
+    }
+
+    public String getSegundoApellido() {
+        return segundoApellido;
+    }
+
+    public String getNacionalidad() {
+        return nacionalidad;
+    }
+
+    public String getNombreCompleto() {
+        return (primerNombre + " " +
+                (segundoNombre != null ? segundoNombre + " " : "") +
+                primerApellido + " " +
+                (segundoApellido != null ? segundoApellido : "")).trim();
+    }
+
 }
